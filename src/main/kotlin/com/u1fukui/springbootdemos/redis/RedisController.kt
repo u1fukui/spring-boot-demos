8c03df2dd2b7aa18f7435516994c2c6b7fd0126d
u1 <u1.kobayashi@gmail.com>
2019-01-27 16:09:41 +0900

    Implement count up API with Redis

--- src/main/kotlin/com/u1fukui/springbootdemos/redis/RedisController.kt
+++ src/main/kotlin/com/u1fukui/springbootdemos/redis/RedisController.kt
@@ -1,11 +1,24 @@
 package com.u1fukui.springbootdemos.redis
 
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RedisController {
 
-    @GetMapping("/redis/hello")
-    fun hello(): String = "Hello World"
+    @Autowired
+    lateinit var redisTemplate: StringRedisTemplate
+
+    @PostMapping("/redis/countup")
+    fun count(): Long? {
+        val count = redisTemplate.opsForValue().increment(KEY_COUNT)
+        redisTemplate.opsForValue().set(KEY_COUNT, count.toString())
+        return count
+    }
+
+    companion object {
+        private const val KEY_COUNT = "count:v1"
+    }
 }
